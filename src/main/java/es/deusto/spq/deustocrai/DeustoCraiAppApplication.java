1b7e83806d82e1949adb08e453c61be08c347e65
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2026-03-13 13:56:06 +0100

    Cambio de estructura

---
+++ src/main/java/es/deusto/spq/deustocrai/DeustoCraiAppApplication.java
@@ -0,0 +1,13 @@
+package es.deusto.spq.deustocrai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeustoCraiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeustoCraiAppApplication.class, args);
+	}
+
+}
