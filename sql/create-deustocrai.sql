6c7c3211b03d968e23a9d4b738f2a91663755236
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2026-03-13 14:00:55 +0100

    Update create-deustocrai.sql

---
+++ sql/create-deustocrai.sql
@@ -0,0 +1,5 @@
+DROP SCHEMA IF EXISTS deustocrai_db;
+DROP USER IF EXISTS 'spq'@'%';
+CREATE SCHEMA deustocrai_db;
+CREATE USER IF NOT EXISTS 'spq'@'%' IDENTIFIED BY 'spq';
+GRANT ALL ON deustocrai_db.* TO 'spq'@'%';
