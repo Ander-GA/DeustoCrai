e64768d0dbc5e819cb723898e8c0347464ca1aec
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2026-03-16 12:32:05 +0100

    Implementacion de lombok

--- src/main/java/es/deusto/spq/deustocrai/DeustoCraiAppApplication.java
+++ src/main/java/es/deusto/spq/deustocrai/DeustoCraiAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeustoCraiAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeustoCraiAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeustoCraiAppApplication.class, args);
+    }
 
 }
