427d22545e044bfa7912128a9b4316dc0b3993ed
Emilio Gil del Rio <emilio.gildelrio@opendeusto.es>
2026-03-17 12:53:35 +0100

    aula controler

--- sql/create-deustocrai.sql
+++ sql/create-deustocrai.sql
@@ -3,3 +3,7 @@ DROP USER IF EXISTS 'spq'@'%';
 CREATE SCHEMA deustocrai_db;
 CREATE USER IF NOT EXISTS 'spq'@'%' IDENTIFIED BY 'spq';
 GRANT ALL ON deustocrai_db.* TO 'spq'@'%';
+
+-- datos de prueba:
+INSERT INTO deustocrai_db.aula (nombre, capacidad, tiene_proyector) VALUES ('Sala de Juntas', 10, true);
+INSERT INTO deustocrai_db.aula (nombre, capacidad, tiene_proyector) VALUES ('Aula 102', 30, false);
---
+++ src/main/java/es/deusto/spq/deustocrai/controller/AulaController.java
@@ -0,0 +1,48 @@
+package es.deusto.spq.deustocrai.controller;
+
+import es.deusto.spq.deustocrai.entity.Aula;
+import es.deusto.spq.deustocrai.entity.Reserva;
+import es.deusto.spq.deustocrai.repository.AulaRepository;
+import es.deusto.spq.deustocrai.repository.ReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/salas")
+public class AulaController {
+
+    @Autowired
+    private AulaRepository aulaRepository;
+
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    // Tarea #60: Consultar todas las salas y su estado
+    @GetMapping
+    public List<Aula> listarSalas() {
+        return aulaRepository.findAll();
+    }
+
+    // Tarea #60: Consultar reservas de una sala específica
+    @GetMapping("/{id}/reservas")
+    public List<Reserva> consultarReservas(@PathVariable Long id) {
+        return reservaRepository.findByAulaId(id);
+    }
+
+    // Tarea #59: Crear una nueva sala (para admins)
+    @PostMapping
+    public Aula crearSala(@RequestBody Aula aula) {
+        return aulaRepository.save(aula);
+    }
+
+    // Tarea #59: Realizar una reserva
+    @PostMapping("/reservar")
+    public ResponseEntity<Reserva> reservarSala(@RequestBody Reserva reserva) {
+        // Aquí podrías añadir lógica para verificar si el aula está libre en ese horario
+        Reserva nuevaReserva = reservaRepository.save(reserva);
+        return ResponseEntity.ok(nuevaReserva);
+    }
+}
