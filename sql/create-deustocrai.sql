f4925ae5e289ed9d7f51d47207db09a7643ed202
Emilio Gil del Rio <emilio.gildelrio@opendeusto.es>
2026-03-17 14:33:08 +0100

    sql

--- sql/create-deustocrai.sql
+++ sql/create-deustocrai.sql
@@ -4,6 +4,9 @@ CREATE SCHEMA deustocrai_db;
 CREATE USER IF NOT EXISTS 'spq'@'%' IDENTIFIED BY 'spq';
 GRANT ALL ON deustocrai_db.* TO 'spq'@'%';
 
--- datos de prueba:
-INSERT INTO deustocrai_db.aula (nombre, capacidad, tiene_proyector) VALUES ('Sala de Juntas', 10, true);
-INSERT INTO deustocrai_db.aula (nombre, capacidad, tiene_proyector) VALUES ('Aula 102', 30, false);
+-- Datos de prueba para la Tarea #61:
+INSERT INTO deustocrai_db.aula (nombre, capacidad, tiene_proyector) 
+VALUES ('Sala de Juntas', 10, true);
+
+INSERT INTO deustocrai_db.aula (nombre, capacidad, tiene_proyector) 
+VALUES ('Aula 102', 30, false);
