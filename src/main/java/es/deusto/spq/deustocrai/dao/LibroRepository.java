72a41675ac3ccbe4e2aae3c427c808ac2479f426
ggredilla <gaizka.gredilla@opendeusto.es>
2026-03-24 13:18:44 +0100

    Ampliar repositorios para que sepa buscar por titulo

--- src/main/java/es/deusto/spq/deustocrai/dao/LibroRepository.java
+++ src/main/java/es/deusto/spq/deustocrai/dao/LibroRepository.java
@@ -1,5 +1,7 @@
 package es.deusto.spq.deustocrai.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import es.deusto.spq.deustocrai.entity.Libro;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
+	List<Libro> findByTituloContainingIgnoreCase(String titulo);
 }
