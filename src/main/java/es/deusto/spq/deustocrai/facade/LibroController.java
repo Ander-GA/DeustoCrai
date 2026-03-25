90aa5399cc5cada12b38e596acfbba75a6dcf85c
ggredilla <gaizka.gredilla@opendeusto.es>
2026-03-25 23:15:08 +0100

    Solucionar error libros

--- src/main/java/es/deusto/spq/deustocrai/facade/LibroController.java
+++ src/main/java/es/deusto/spq/deustocrai/facade/LibroController.java
@@ -1,5 +1,6 @@
 package es.deusto.spq.deustocrai.facade;
 
+import es.deusto.spq.deustocrai.dao.LibroRepository;
 import es.deusto.spq.deustocrai.entity.Libro;
 import es.deusto.spq.deustocrai.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional; 
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/libros")
@@ -17,13 +18,46 @@ public class LibroController {
     @Autowired
     private LibroService libroService;
 
-    
+    @Autowired
+    private LibroRepository libroRepository; 
+
+ 
+    @GetMapping
+    public List<Libro> listarLibros() {
+        return libroService.listarLibros();
+    }
+
+   
+    @GetMapping("/buscar")
+    public List<Libro> buscarLibros(@RequestParam("q") String q) {
+        return libroRepository.findByTituloContainingIgnoreCase(q);
+    }
+
+   
     @GetMapping("/{id}")
     public ResponseEntity<Libro> obtenerDetallesLibro(@PathVariable("id") Long id) {
         Optional<Libro> libro = libroService.obtenerLibroPorId(id);
         
         if (libro.isPresent()) {
             return ResponseEntity.ok(libro.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+   
+    @PostMapping
+    public ResponseEntity<Libro> anadirLibro(@RequestBody Libro libro) {
+        Libro nuevo = libroService.anadirLibro(libro);
+        return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
+    }
+
+  
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> borrarLibro(@PathVariable Long id) {
+        boolean borrado = libroService.borrarLibro(id);
+        if (borrado) {
+            return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
         }
