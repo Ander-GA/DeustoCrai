b0b2dd221ec04b43faeea19e6be2d9ffb75b8b66
Jacqueline Furelos <jacqueline.furelos@opendeusto.es>
2026-04-28 16:10:51 +0200

    Añadido catalogo de materiales

--- src/main/java/es/deusto/spq/deustocrai/facade/MaterialController.java
+++ src/main/java/es/deusto/spq/deustocrai/facade/MaterialController.java
@@ -16,26 +16,27 @@ public class MaterialController {
     @Autowired
     private MaterialRepository materialRepository;
 
-    @PostMapping
-    public ResponseEntity<Material> crearMaterial(@RequestBody Material material) {
-        Material nuevo = materialRepository.save(material);
-        return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
-    }
-
+    // 1. Listar todos los materiales (Lo usa catalogo-material.html)
     @GetMapping
     public List<Material> listar() {
         return materialRepository.findAll();
     }
 
-    
+    // 2. Obtener detalles de uno solo (Lo usa detalle-material.html)
     @GetMapping("/{id}")
     public ResponseEntity<Material> obtenerDetallesMaterial(@PathVariable("id") Long id) {
         Optional<Material> material = materialRepository.findById(id);
-        
         if (material.isPresent()) {
             return ResponseEntity.ok(material.get()); 
         } else {
             return ResponseEntity.notFound().build(); 
         }
+    }
+
+    // 3. Crear material (Opcional, para el administrador)
+    @PostMapping
+    public ResponseEntity<Material> crearMaterial(@RequestBody Material material) {
+        Material nuevo = materialRepository.save(material);
+        return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
     }
 }
